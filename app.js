92ff527f0badfc8ada3d94e2f134fd1da6e86685
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-06-05 09:42:57 +0400

    [Lesson-2] modifires

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const app = Vue.createApp({
     data() {
        return {
-        counter: 0,
+           counter: 0,
+           name: '',
+           confirmedName: '',
        }
     },
 
     methods: {
+        confirmInput() {
+            this.confirmedName = this.name;
+        },
+        submitFrom(event) {
+            alert('Submited!');
+        },
+        setName(event, name) {
+            this.name = event.target.value + ' ' + name;
+        },
         add(num) {
             this.counter = this.counter + num;
         },
