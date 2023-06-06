44e42e0c97f3a29a8d7650f10f0d26b8ae0c9328
hasmikmejlumyan <hasmik.mejlumyan@esterox.am>
2023-06-06 10:05:46 +0400

    [Lesson-2] dynamic styling

--- app.js
+++ app.js
@@ -1,59 +1,29 @@
 const app = Vue.createApp({
     data() {
        return {
-           counter: 10,
-           name: '',
-           confirmedName: '',
-           // fullname: '',
-           lastName: '',
-       }
-    },
-
-    watch: {
-        // name(value) {
-        //     if (value === '') {
-        //         this.fullname = '';
-        //     } else {
-        //         this.fullname = value + ' ' + 'Hasmik';
-        //     }
-        // }
+           boxASelected: false,
+           boxBSelected: false,
+           boxCSelected: false,
+       };
     },
 
     computed: {
-        fullname() {
-            if (this.name === ''){
-                return '';
-            }
-            return this.name + ' ' + this.lastName;
+        boxAClasses() {
+            return {active: this.boxASelected};
         }
     },
 
     methods: {
-        confirmInput() {
-            this.confirmedName = this.name;
-        },
-        submitFrom(event) {
-            alert('Submited!');
-        },
-        outputFullname() {
-            if (this.name === ''){
-                return '';
+        boxSelected (box) {
+            if (box === 'A') {
+                this.boxASelected = !this.boxASelected;
+            } else if (box === 'B') {
+                this.boxBSelected = !this.boxBSelected;
+            } else if (box === 'C') {
+                this.boxCSelected = !this.boxCSelected;
             }
-            return this.name + ' ' + 'Mejlumyan';
-        },
-        setName(event) {
-            this.name = event.target.value;
-        },
-        add(num) {
-            this.counter = this.counter + num;
-        },
-        reduce(num) {
-            this.counter = this.counter - num;
-        },
-        resetInput() {
-            this.name = '';
-        },
+        }
     }
 });
 
-app.mount('#events');
+app.mount('#styling');
